73b1dd322400cb582cce62fd9d0528a2249d7247
Kashif Masood <kashif.masood@davita.om>
2017-12-20 09:40:39 -0700

    Implementation for identifying & recording internal and external links.

--- src/main/java/com/kmasood/model/PageLinks.java
+++ src/main/java/com/kmasood/model/PageLinks.java
@@ -4,18 +4,42 @@ import java.util.HashSet;
 
 public class PageLinks {
 
+  private String rootDomain;
   private HashSet<String> internalLinks;
   private HashSet<String> externalLinks;
   private HashSet<String> staticLinks;
 
-  public PageLinks() {
+  public PageLinks(String rootDomainURL) {
+    rootDomain = rootDomainURL;
     internalLinks = new HashSet<>();
     externalLinks = new HashSet<>();
     staticLinks = new HashSet<>();
   }
 
   public void addLink(String url) {
+    if (url.startsWith(rootDomain)) {
+      internalLinks.add(url);
+    } else {
+      externalLinks.add(url);
+    }
+  }
 
+  public boolean alreadyVisited(String url) {
+    boolean foundIt = false;
+
+    if (internalLinks.contains(url)) {
+      foundIt = true;
+    } else if (externalLinks.contains(url)) {
+      foundIt = true;
+    } else if (staticLinks.contains(url)) {
+      foundIt = true;
+    }
+
+    return foundIt;
+  }
+
+  public boolean isExternalLink(String url) {
+    return (url.startsWith(rootDomain) ? false : true);
   }
 
   public HashSet<String> getInternalLinks() {
--- src/test/java/com/kmasood/crawler/CrawlerTest.java
+++ src/test/java/com/kmasood/crawler/CrawlerTest.java
@@ -8,27 +8,24 @@ import static org.junit.Assert.*;
 
 public class CrawlerTest {
 
-  private Crawler crawler;
-
   @Before
   public void setUp() throws Exception {
-    crawler = new Crawler();
   }
 
   @Test
   public void testWebsite1() {
-    crawler.parsePage("https://kashifmasood.github.io/WebCrawler/html/SamplePage1.html");
+    Crawler crawler = new Crawler("https://kashifmasood.github.io/WebCrawler/html/SamplePage1.html");
 
     PageLinks pagelinks  = crawler.getPageLinks();
 
-    assertEquals("External Links", 5, pagelinks.getExternalLinks().size());
-    assertEquals("Internal Links", 0, pagelinks.getInternalLinks().size());
+    assertEquals("External Links", 2, pagelinks.getExternalLinks().size());
+    assertEquals("Internal Links", 1, pagelinks.getInternalLinks().size());
     assertEquals("Static Links", 3, pagelinks.getStaticLinks().size());
   }
 
   @Test
   public void testWebsite2() {
-    crawler.parsePage("https://kashifmasood.github.io/WebCrawler/html/SamplePage2.html");
+    Crawler crawler = new Crawler("https://kashifmasood.github.io/WebCrawler/html/SamplePage2.html");
 
     PageLinks pagelinks  = crawler.getPageLinks();
 
