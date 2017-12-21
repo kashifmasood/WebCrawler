18315a80ff72c1af8fef18f0a8605c78d0b9593e
Kashif Masood <kashif.masood@davita.om>
2017-12-21 13:38:20 -0700

    Updated documentation

--- src/test/java/com/kmasood/crawler/CrawlerTest.java
+++ src/test/java/com/kmasood/crawler/CrawlerTest.java
@@ -8,6 +8,24 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
+/**
+ * Crawler test class
+ *
+ * NOTE: All the test cases below uses the html pages in GitHub Pages for the kmasood account: WebCrawler repo.
+ * This was done to ensure that testing is done in a real life scenario of a web page and not a local static page
+ * (specially when crawling internal pages).
+ *
+ * The HTML pages used below are part for the GIT repo under the following location:
+ *      WebCrawler/html/
+ *
+ * The HTML pages are published to the GitHub Pages website from the MASTER branch only.
+ *
+ * If you want to add more test data to the HTML pages, please make sure you push your changes to the MASTER
+ * branch before running your test cases.
+ *
+ * @author kmasood
+ *
+ */
 public class CrawlerTest {
 
   /**
@@ -21,10 +39,12 @@ public class CrawlerTest {
 
     List<PageLinks> list = crawler.getPageLinksList();
 
+    // We should be visiting any external links
     assertEquals("# Of Pages", 1, list.size());
 
     PageLinks pageLinks = list.get(0);
     assertNotNull("No page data found", pageLinks);
+
     assertEquals("External Links", 2, pageLinks.getExternalLinks().size());
     assertEquals("Internal Links", 0, pageLinks.getInternalLinks().size());
     assertEquals("Static Links", 3, pageLinks.getStaticLinks().size());
@@ -41,10 +61,12 @@ public class CrawlerTest {
 
     List<PageLinks> list = crawler.getPageLinksList();
 
+    // We should be visiting any external links
     assertEquals("# Of Pages", 1, list.size());
 
     PageLinks pageLinks = list.get(0);
     assertNotNull("No page data found", pageLinks);
+
     assertEquals("External Links",3, pageLinks.getExternalLinks().size());
     assertEquals("Internal Links",3, pageLinks.getInternalLinks().size());
     assertEquals("Static Links",2, pageLinks.getStaticLinks().size());
